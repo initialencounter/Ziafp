64a510fe9dc8a6e7287d807b0f50cf37164fc112
initialencounter <2911583893@qq.com>
2024-10-30 17:30:42 +0800

    add path parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use std::io;
+use std::path::PathBuf;
 use winreg::enums::*;
 use winreg::RegKey;
 use std::env;
 use reqwest::blocking::{Client, multipart};
+use std::fs;
+use std::path::Path;
 
+fn traverse_directory(path: &Path, depth: usize) {
+    if depth > 3 {
+        return;
+    }
+    if path.is_dir() {
+        for entry in fs::read_dir(path).unwrap() {
+            let entry_path = entry.unwrap().path();
+            // println!("当前路径: {:?}", entry_path);
+            parse_path(entry_path.clone());
+            traverse_directory(&entry_path, depth + 1);
+        }
+    }
+}
+
+fn main() {
+    let start_path = Path::new(r"E:\2024\10\00000000000000000000000000\10.30优瑞特（实达 9964145）\新出空运 ORTB21-2024082002 佛山市实达科技 9964145 优瑞特 10.30 贲");
+    traverse_directory(start_path, 1);
+}
 fn create_reg(key_name: &str, menu_name: &str, command: &str, icon: &str) -> io::Result<()> {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let vs_code_key = hklm.create_subkey(format!("SOFTWARE\\Classes\\Directory\\background\\shell\\{}", key_name))?;
@@ -16,6 +37,14 @@ fn create_reg(key_name: &str, menu_name: &str, command: &str, icon: &str) -> io:
 }
 pub fn is_integer(s: &str) -> bool {
     s.parse::<i32>().is_ok()
+}
+
+fn parse_path(path: PathBuf) -> () {
+    let dir = path.as_os_str().to_str().unwrap().to_string();
+    let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
+    if file_name.ends_with(".pdf") && file_name.starts_with("PEKGZ2024") {
+        println!("文件名称: {:?}", file_name);
+    }
 }
 
 
@@ -39,7 +68,7 @@ pub fn post_file(file_path: String) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn main() {
+fn maina() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     if args.len() < 2 {
